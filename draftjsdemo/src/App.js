214fb778dd15ed162bfc5aaeb606ccd16c9080a4
Msspl-PrashenJeetRoy <prashenjeet.roy@massoftind.com>
2018-01-12 20:11:22 +0530

    updated files

--- draftjsdemo/src/App.js
+++ draftjsdemo/src/App.js
@@ -1,46 +1,99 @@
 import React, { Component } from 'react';
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import {
+  ImageSideButton,
+  Block,
+  addNewBlock,
+  createEditorState,
+  Editor,
+} from 'medium-draft';
+import 'isomorphic-fetch';
+class CustomImageSideButton extends ImageSideButton {
 
+  /*
+  We will only check for first file and also whether
+  it is an image or not.
+  */
+  onChange(e) {
+    const file = e.target.files[0];
+    if (file.type.indexOf('image/') === 0) {
+      // This is a post request to server endpoint with image as `image`
+      const formData = new FormData();
+      formData.append('image', file);
+      fetch('/your-server-endpoint', {
+        method: 'POST',
+        body: formData,
+      }).then((response) => {
+        if (response.status === 200) {
+          // Assuming server responds with
+          // `{ "url": "http://example-cdn.com/image.jpg"}`
+          return response.json().then(data => {
+            if (data.url) {
+              this.props.setEditorState(addNewBlock(
+                this.props.getEditorState(),
+                Block.IMAGE, {
+                  src: data.url,
+                }
+              ));
+            }
+          });
+        }
+      });
+    }
+    this.props.close();
+  }
+
+}
+
+class Testyo extends Component {
+  render(){
+    return(
+      <p>sdjflksjsdjfsd</p>
+      )
+  }
+}
+
+// Now pass this component instead of default prop to Editor example above.
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+
+    this.sideButtons = [{
+      title: 'Image',
+      component: CustomImageSideButton,
+    }];
+
+    this.state = {
+      editorState: createEditorState(), // for empty content
+    };
+
+    /*
+    this.state = {
+      editorState: createEditorState(data), // with content
+    };
+    */
+
+    this.onChange = (editorState) => {
+      this.setState({ editorState });
+    };
   }
 
-  handleKeyCommand(command, editorState) {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
+  componentDidMount() {
+    this.refs.editor.focus();
   }
-
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  }_
-  _onItalicClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
-  }
-
 
   render() {
+    const { editorState } = this.state;
     return (
-      <div>
-
-        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-        <button onClick={this._onItalicClick.bind(this)}>Italic</button>
-         <Editor
-          editorState={this.state.editorState}
-          handleKeyCommand={this.handleKeyCommand}
-          onChange={this.onChange}
+      <Editor
+        ref="editor"
+        editorState={editorState}
+        onChange={this.onChange}
+        sideButtons={this.sideButtons}
       />
-      </div>
     );
   }
-}
+};
+
 
 
 export default App;
